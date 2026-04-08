35e3152fbae45a644c9557688458eb8608e23a66
xvysot00 <xvysot00@vutbr.cz>
2026-04-08 15:32:59 +0300

    feat: add TaskManager with save_to_file method

--- main.py
+++ main.py
@@ -12,7 +12,17 @@ class Task:
         status = "done" if self.is_done else "in progres"
         return f"[{self.task_id}] {status} Priority: {self.priority} | {self.description} (Created: {self.created_at})"
 
+class TaskManager:
+    def __init__(self, filename="tasks.txt"):
+        self.filename = filename
+
+    def save_to_file(self, task):
+        with open(self.filename, "a", encoding="utf-8") as f:
+            line = f"{task.task_id}|{task.description}|{task.priority}|{task.created_at}|{task.is_done}\n"
+            f.write(line)
+        print(f"Task '{task.description}' saved to tasks.txt")
+
 if __name__ == "__main__":
-    sample_task = Task(1, "Study ООP", 1)
-    print("It works")
-    print(sample_task)
+    manager = TaskManager()
+    new_task = Task(1, "Study ООP", 3)
+    manager.save_to_file(new_task)
